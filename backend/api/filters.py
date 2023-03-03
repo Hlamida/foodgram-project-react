d20db005f6d74a7292f8d4068c8496f4f627cfb1
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-03-03 21:35:27 +0300

    Deploy-103

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,38 +1,38 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Recipe
+from recipes.models import Favorite, Recipe, Tag
 from rest_framework.filters import SearchFilter
-from users.models import User
 
 
 class RecipesFilter(FilterSet):
     """Класс для фильтрации рецептов."""
 
-    tags = filters.AllValuesMultipleFilter(field_name='tags__recipetags__slug')
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = {
-            'tags',
-            'author',
-        }
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-
-        if value and not self.request.user.is_anonymous:
-            return queryset.filter(favorite__user=self.request.user)
-
-        return queryset
+        if not value:
+            return queryset
+        favorite_recipes_id = Favorite.objects.filter(
+            user=self.request.user.id).values_list("recipe__id", flat=True)
+        return queryset.filter(id__in=favorite_recipes_id)
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-
-        if value and not self.request.user.is_anonymous:
-            return queryset.filter(cart__user=self.request.user)
-
-        return queryset
+        if not value:
+            return queryset
+        shoppingcart_recipes_id = Favorite.objects.filter(
+            user=self.request.user.id).values_list("recipe__id", flat=True)
+        return queryset.filter(id__in=shoppingcart_recipes_id)
 
 
 class IngredientsFilter(SearchFilter):
