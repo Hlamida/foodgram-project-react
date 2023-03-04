303143f89b06ddb2add0f38afbef86cf1e54d5a4
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-03-04 15:17:08 +0300

    Deploy-106

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -7,32 +7,19 @@ from users.models import User
 class RecipesFilter(FilterSet):
     """Класс для фильтрации рецептов."""
 
-    tags = filters.AllValuesMultipleFilter(field_name='tags__recipetags__slug')
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+        widget=filters.widgets.BooleanWidget(), label='В корзине.'
+    )
+    is_favorited = filters.BooleanFilter(
+        widget=filters.widgets.BooleanWidget(), label='В избранных.'
+    )
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug',
+                                           label='Ссылка')
 
     class Meta:
         model = Recipe
-        fields = {
-            'tags',
-            'author',
-        }
-
-    def filter_is_favorited(self, queryset, name, value):
-
-        if value and not self.request.user.is_anonymous:
-            return queryset.filter(favorite__user=self.request.user)
-
-        return queryset
-
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-
-        if value and not self.request.user.is_anonymous:
-            return queryset.filter(cart__user=self.request.user)
-
-        return queryset
+        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
 
 
 class IngredientsFilter(SearchFilter):
