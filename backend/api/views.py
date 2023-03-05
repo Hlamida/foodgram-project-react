82ce19fb0a100ca7c2c46010456e84af03e1ff76
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-03-05 22:13:10 +0300

    Deploy-155

--- backend/api/views.py
+++ backend/api/views.py
@@ -141,8 +141,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
         if 'is_in_shopping_cart' in self.request.query_params:
             return Recipe.objects.filter(cart__user=self.request.user)
 
-        #return Recipe.objects.filter(tags__slug__in=query_tags)
-        return Recipe.objects.all()
+        return Recipe.objects.filter(tags__slug__in=query_tags).distinct()
 
     def perform_create(self, serializer):
         """Передает сериализатору автора рецепта."""
