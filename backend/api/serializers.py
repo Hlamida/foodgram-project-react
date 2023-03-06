bc1e623fb9e298feeba14f9749977e3c26bb6889
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-03-06 22:32:52 +0300

    Deploy-174

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -164,14 +164,13 @@ class RecipeListSerializer(serializers.ModelSerializer):
         """Изменяет рецепт."""
 
         ingredients_data = validated_data.pop('ingredients')
-        super().update(instance, validated_data)
-        #RecipeIngredients.objects.filter(
-        #    recipe=instance
-        #).delete()
+        RecipeIngredients.objects.filter(
+            recipe=instance
+        ).delete()
         self.add_ingredients(ingredients_data, instance)
         instance.save()
 
-        return instance
+        return super().update(instance, validated_data)
 
     #def update(self, instance, validated_data):
     #    ingredients = validated_data.pop('ingredients')
